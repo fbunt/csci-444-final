1fc08c7acec460bcead56924d31b5a66996ba0d2
Fred Bunt <fredrick.bunt@gmail.com>
2018-12-11 21:30:39 -0700

    Add scale label and adjust right margin

--- src/plotnc.sh
+++ src/plotnc.sh
@@ -29,6 +29,7 @@ w=7i
 
 gmt grdimage $infile -Rg -JKs180/$w -B+t"$title" -Csst.cpt -P -K > $framefile
 gmt pscoast -Rg -JKs180/$w -Bafg -Dc -A5000 -Wthinnest -Gwhite -O -K >> $framefile
-gmt psscale -Rg -JKs180/$w -DjMR+w4i/0.5+v+o-1i/0i -Csst.cpt -Ba6f -O >> $framefile
+gmt psscale -Rg -JKs180/$w -DJMR+w4i/0.5+v+o0.75i/0i+ma \
+    -Bx+l"Sea Surface Temp. (\260C)" --FONT_LABEL=11p -Csst.cpt -Ba6f -O >> $framefile
 # -A: L/R/B/T
-gmt psconvert $framefile -P -E300 -Tg -A0.3i/1i/0.5i/0.3i -Z
+gmt psconvert $framefile -P -E300 -Tg -A0.3i/1.2i/0.5i/0.3i -Z
